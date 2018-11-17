f81edc29e64c680dabb399179d0d6c0185a5365e
mohamedhamdy2017 <34789872+mohamedhamdy2017@users.noreply.github.com>
2018-11-17 16:38:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { SearchBar } from 'react-native-elements';
 export default class App extends Component{
     state={
         data: [],
-        query: '',
         loadingIcon: false
     }
 
@@ -16,12 +15,13 @@ export default class App extends Component{
         fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${search}`)
         .then(response => response.json())
         .then(result => this.setState({
-            data: result[0]
+            data: result[1]
         }))
     }
+    
 
-    renderItem = ({item}) => {
-        <View>
+    renderItem = ({item, index}) => {
+        <View key={index}>  
             <Text>{item}</Text>
         </View>
     }
@@ -34,34 +34,17 @@ export default class App extends Component{
             <SearchBar
                 lightTheme
                 round
-                onChangeText ={(search) => this.onTyping({search})}
+                onChangeText={(search) => {this.onTyping(search)}}
                 placeholder='Type Here...' 
                 showLoadingIcon = {false}
                 />
            <FlatList
             data={this.state.data}
             renderItem={this.renderItem}
+            keyExtractor={item => item}
             />
       </View>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
