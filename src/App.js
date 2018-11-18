5bc3e717febd46254e89e488d67f810a90684d67
mohamedhamdy2017 <34789872+mohamedhamdy2017@users.noreply.github.com>
2018-11-18 14:57:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
-import {
-  View, FlatList, Text
-} from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, { Component } from 'react'
+import { FlatList, Text, View, TouchableOpacity, ActivityIndicator, Linking } from 'react-native'
+// Styles
+import { CardItem, Body } from 'native-base'
+import { SearchBar } from 'react-native-elements'
 
-
-export default class App extends Component{
-    state={
+export default class LaunchScreen extends Component {
+    
+    state = {
         data: [],
-        loadingIcon: false
-    }
-
-    onTyping = (search) => {
-        fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${search}`)
-        .then(response => response.json())
-        .then(result => this.setState({
-            data: result[1]
-        }))
+        discription: [],
+        Link: null,
+        loading: false
     }
     
-
-    renderItem = ({item, index}) => {
-        <View key={index}>  
-            <Text>{item}</Text>
-        </View>
+    onTyping = (search) => {
+        fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${search}`)
+            .then(response => response.json())
+            .then(result => this.setState({
+                loading: true,
+                data: result[1],
+                discription: result[2],
+                Link: result[3],
+                loading: false
+            }))
     }
-
-
-
-  render() {
-    return (
-      <View >
-            <SearchBar
-                lightTheme
-                round
-                onChangeText={(search) => {this.onTyping(search)}}
-                placeholder='Type Here...' 
-                showLoadingIcon = {false}
+   
+    
+    renderItem = ({item}) => (
+            <CardItem >
+              <Body style={{backgroundColor: '#eee', borderWidth:0.4}} >
+              <Text style={{margin: 10, fontWeight: 'bold'}}>{item}</Text>
+                <TouchableOpacity onPress ={() => Linking.openURL(this.state.Link)}>
+                    <Text style={{margin: 10}}>{this.state.discription}</Text>
+                </TouchableOpacity>
+              </Body>
+            </CardItem>    
+    )
+    
+    render () {
+        const{loading} = this.state;
+        return (
+            <View>
+                <SearchBar 
+                    round
+                    lightTheme
+                    placeholder="Type Here..."
+                    onChangeText={this.onTyping}
                 />
-           <FlatList
-            data={this.state.data}
-            renderItem={this.renderItem}
-            keyExtractor={item => item}
-            />
-      </View>
-    );
-  }
+                {loading? 
+                <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+                    <ActivityIndicator size="large" color={'#9bfb8c'} />
+                </View>:
+                <FlatList
+                    data={this.state.data}
+                    renderItem={this.renderItem}
+                    keyExtractor={item => item}
+                />
+            }
+            </View>
+        )
+    }
 }
-
